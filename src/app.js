982bf22529620ba597f6d2402e0a54e3c0905c8b
Viacheslav <wooofah@gmail.com>
2020-07-09 13:58:39 +0300

    setup pp for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const PORT = process.env.PORT || 3000
+
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/patrials')
 
@@ -91,6 +93,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server is running..')
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
 })
